de05998a7e6462c20cc80b8582cbe26fdd26a4fe
Jiwon <jmjyjw1@gmail.com>
2021-01-02 19:49:35 +0900

    210102_설탕배달 수정
    최종 수정.

--- SugarDelivery_2839.py
+++ SugarDelivery_2839.py
@@ -1,18 +1,14 @@
 # 백준 2839
 
 def sugar(kilo):
-    n = kilo//5 #5kg의 개수를 최대로 했을 때
-    if (kilo - n*5)%3 == 0: #3kg 하나 또는 0개로 N이 완성되는 경우
-        return n + (kilo - n*5)//3 
-    else:
-        rem = kilo % 5
-        temp = kilo - rem
-        while (temp != 0): #5의 배수로 줄여가며 5kg 봉지를 최대로 유지하며
-            if (kilo-temp) % 3 == 0: #3의 배수와의 합으로 N이 완성되는 경우
-                return temp//5 + (kilo-temp)//3
-            temp -= 5
-        if kilo % 3 == 0: #5kg짜리 없이 3kg 만으로 N이 완성되는 경우
-            return kilo // 3
+    rem = kilo % 5 #temp를 N이하 최대의 5의 배수로 만들어주기 위해
+    temp = kilo - rem
+    while (temp != 0): #temp가 0이 되기전까지 5씩 감소시키며 temp를 5의 배수로 유지
+        if (kilo - temp) % 3 == 0: #5kg와 3kg의 조합으로 N이 완성되는 경우
+            return temp//5 + (kilo-temp)//3 #각각의 개수를 반환
+        temp -= 5
+    if kilo % 3 == 0: #5kg짜리 없이 3kg 만으로 N이 완성되는 경우
+        return kilo // 3
     return -1 #5kg와 3kg로 N을 완성할 수 없다면
 
 N = int(input())
