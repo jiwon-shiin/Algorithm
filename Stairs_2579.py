b94f5cabbe64ae7e6cf6fc1e4162f5f96298fb96
Jiwon <jmjyjw1@gmail.com>
2021-01-02 19:50:52 +0900

    210102
    계단오르기 2579

---
+++ Stairs_2579.py
@@ -0,0 +1,20 @@
+# 백준 2579
+
+def stairs(S):
+    DP = [0]*len(S)
+    if len(S) == 1: return 
+    if len(S) == 2: return S[1]
+    if len(S) == 3: return S[1]+S[2]
+    DP[1] = S[1]
+    DP[2] = S[2]+S[1]
+    for i in range(3,len(S)):
+        DP[i] = max(DP[i-2]+S[i], DP[i-3]+S[i-1]+S[i])
+                
+    return DP[-1]
+
+N = int(input())
+S = [0]
+for i in range(N):
+    score = int(input())
+    S.append(score)
+print(stairs(S))
