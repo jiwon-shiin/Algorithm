9f39dd5aa2ba98a234f53b6fe09357846ffab43c
Jiwon <jmjyjw1@gmail.com>
2021-01-05 23:36:23 +0900

    210105
    회의실 배정 1931

--- MeetingRooms_1931.py
+++ MeetingRooms_1931.py
@@ -8,11 +8,10 @@ for i in range(N):
 
 meeting = sorted(meeting, key = lambda x:(x[1],x[0]))
 
-s,e = 0,0
+e = 0
 done_meetings = []
 for i in range(len(meeting)):
     if meeting[i][0] >= e:
-        s = meeting[i][0]
         e = meeting[i][1]
         done_meetings.append(meeting[i])
 print(len(done_meetings))
