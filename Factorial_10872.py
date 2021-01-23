2c682e288c89c30571d3fed65a56831f6a8961e6
Jiwon <jmjyjw1@gmail.com>
2021-01-23 23:42:07 +0900

    210123
    복습 및 주석 수정

--- Factorial_10872.py
+++ Factorial_10872.py
@@ -1,8 +1,8 @@
-# 백준 10872
+# 백준 10872 팩토리얼 (신지원)
 
 def factorial(n):
-    if n <= 1: return 1
-    return n * factorial(n-1)
+    if n <= 1: return 1 #base case
+    return n * factorial(n-1) #재귀적으로 실행
 
 n = int(input())
-print(factorial(n))
+print(factorial(n)) #결과를 출력
